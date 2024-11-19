288388f839a35c78ffc22afd530a0e8f94c33445
Debargha Ghosh <71061232+DEBG14@users.noreply.github.com>
2024-11-20 00:23:04 +0530

    Update trials.py

--- trials.py
+++ trials.py
@@ -1,19 +1,19 @@
 from BG_SUB import SG_model
 import cv2
 
-
-
-GMM=SG_model(0.3,0.75,3)
+GMM=SG_model(0.008,0.5,3)
+print("start")
 GMM.parameter_init()
+print("initialization complete")
 succ=1
 count=0
 cap=cv2.VideoCapture('umcp.mpg')
 while succ:
     succ,frame=cap.read()
     grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    fore,back=GMM.fit(grayscale,frame)
-    cv2.imwrite(r"D:/fifa 15/test2/f%d.jpg" % count, fore)
-    cv2.imwrite(r"D:/fifa 15/test1/b%d.jpg" % count, back)
+    fore,back = GMM.fit(grayscale,frame)
+    cv2.imwrite(r"D:/CUT_PASTE/bg subtraction/outputs/fore/f%d.jpg" % count, fore)
+    cv2.imwrite(r"D:/CUT_PASTE/bg subtraction/outputs/back/b%d.jpg" % count, back)
     count+=1
 
 
